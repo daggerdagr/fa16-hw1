9d33bd4e63d5c8bc408edc342121fddf109cf3b4
Regina Ongowarsito <regina.ongowarsito@gmail.com>
2016-09-21 04:12:33 -0700

    hw done

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,7 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map{|j| (Integer(j) + 2)}.select{|a| a % 2 == 0}.uniq.select{|a| a <= 10}.reduce(:+)
   end
 end
 
